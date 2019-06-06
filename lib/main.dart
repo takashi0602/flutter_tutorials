8e05161a2b66bdcb70f0617d60daa64551691b3f
takashi <takashioicsn@gmail.com>
2019-06-06 18:58:26 +0900

    Step 6: Final touch

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Column(
+        body: ListView(
           children: [
             Image.asset(
               'images/lake.jpg',
